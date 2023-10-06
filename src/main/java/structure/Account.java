83c24e6423f8f5c50a53410dec8322fcfb9bc682
joeperez11 <perez.joseph.k@gmail.com>
2023-10-06 02:47:37 -0500

    testing pushing

---
+++ src/main/java/structure/Account.java
@@ -0,0 +1,16 @@
+package structure;
+
+public class Account {
+    String owner;
+    double balance;
+    double minimumBalance;
+
+    public void deposit(double amount) {
+        //implementation
+    }
+
+    public void withdraw(double amount){
+
+        //implementation
+    }
+}
