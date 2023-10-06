fb9c274a749a704fe3ef4d291248df1eebb62faa
joeperez11 <perez.joseph.k@gmail.com>
2023-10-06 02:30:16 -0500

    test

---
+++ src/main/java/com/example/jas/JasApplication.java
@@ -0,0 +1,13 @@
+package com.example.jas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jas/JasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
